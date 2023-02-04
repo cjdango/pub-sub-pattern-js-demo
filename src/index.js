e1c5873e523a5220648e3381091fae807f67a7ed
Christian Jay Dango <31501132+cjdango@users.noreply.github.com>
2023-02-04 08:17:33 +0000

    feat: added EventBusFactory implementation

--- src/index.js
+++ src/index.js
@@ -2,15 +2,28 @@ const EventBusFactory = () => {
     const listeners = {};
 
     const fire = (event, payload) => {
-        // publish message here
+        const messages = [];
+
+        if (!listeners[event]) return messages
+
+        for (const listener of listeners[event]) {
+            messages.push(listener(payload))
+        }
+        return messages
     };
 
     const listen = (event, listener) => {
-        // subscribe a message
+        if (!listeners[event]) {
+            listeners[event] = [listener]
+        } else {
+            listeners[event].push(listener)
+        }
+
+        return listener;
     };
 
     const unsubscribe = (event, removeListener) => {
-        // unsubscribe from event
+        listeners[event] = listeners[event]?.filter(l => l !== removeListener)
     };
 
     return {
